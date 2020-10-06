7c624894b59dd83f929ec576ed99da095ad9078f
JuniorDeveloper54 <lotifurshabbir54@gmail.com>
2020-10-06 08:45:37 +0600

    extra code removed

--- src/App.js
+++ src/App.js
@@ -1,83 +1,9 @@
-import React, { createContext } from 'react';
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
-import 'bootstrap/dist/css/bootstrap.css';
-import Header from './Component/Header/Header';
-import GoogleSignIn from './Component/GoogleSignIn/GoogleSignIn';
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
-  Link
-} from "react-router-dom";
-import { useContext } from 'react';
-import Error from './Component/Error/Error';
-import Register from './Component/Register/Register';
-import PrivateRoute from './Component/PrivateRoute/PrivateRoute';
-import { useState } from 'react';
-import ShowUserPersonalInfo from './Component/ShowUserPersonalInfo/ShowUserPersonalInfo';
-import VolunteerList from './Component/VolunteerList/VolunteerList';
-import VolunteerEventAdd from './Component/VolunteerEventAdd/VolunteerEventAdd';
-import { ToastContainer, toast } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
-
-export const UserContext = createContext();
+import Main from './components/Main';
 
 function App() {
-  const [users, setUser] = useState({
-    UserName: "",
-    userEmail: "",
-    issuDate: "",
-    massage: "",
-    category: "",
-    descriptions: "",
-    isAdmin: false,
-  })
-  const [allEventsInfo, setAllEventsInfo] = useState([])
-  return (
-    <UserContext.Provider value={{ userInfo: [users, setUser], eventInfo: [allEventsInfo, setAllEventsInfo] }}>
-      <div className="App-wrapper">
-        <ToastContainer />
-        <Router>
-          <Switch>
-            <Route path="/home" >
-              <Header />
-            </Route>
-            <Route path="/adminVolunteerList" >
-              <VolunteerList />
-            </Route>
-            <Route path="/adminEventsUpdate" >
-              <VolunteerEventAdd />
-            </Route>
-            <PrivateRoute path="/afterRegister" >
-              <Header />
-            </PrivateRoute>
-            <Route path="/admin-panel" >
-              <VolunteerList />
-            </Route>
-            <PrivateRoute path="/event" >
-              <ShowUserPersonalInfo />
-            </PrivateRoute>
-            <Route path="/login" >
-              <GoogleSignIn />
-            </Route>
-            <Route path="/userAllTask" >
-              <ShowUserPersonalInfo />
-            </Route>
-            <PrivateRoute path="/register/:id" >
-              <Register />
-            </PrivateRoute>
-            <Route exact path="/" >
-              <Header />
-            </Route>
-            <Route path="*" >
-              <Error />
-            </Route>
-          </Switch>
-        </Router>
-      </div>
-    </UserContext.Provider >
-  );
+  return <Main></Main>
 }
 
 export default App;
